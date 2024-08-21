390f63dd5890154b25f5dd0906cddeb57e3169f0
atheist31 <rkb0808@gmaol.com>
2024-08-21 20:08:07 +0530

    unary

---
+++ unaryOperator.js
@@ -0,0 +1,34 @@
+// The unary plus (+) operator precedes its operand and evaluates to its operand but attempts to convert it into a number, if it isn't already.
+
+
+// console.log(+"1");
+// console.log(+" ");
+// console.log(+"ravi");
+// console.log(+1);
+
+
+// console.log(typeof(+"1")); /*number*/
+// console.log(typeof(+" "));/*number*/
+// console.log(+"ravi");/*NaN*/
+// console.log(typeof(+1));/*number*/
+
+
+// function ravi(){
+//   if([]){
+//     console.log("True")
+//   }
+//   else{
+//     console.log("False")
+//   }
+// }
+// ravi();
+
+function ravi(){
+    if(""){
+      console.log("True")
+    }
+    else{
+      console.log("False")
+    }
+  }
+  ravi();

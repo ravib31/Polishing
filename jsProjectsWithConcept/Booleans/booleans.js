015223001eafedb49f0c35a0e7e39a45d48e756c
ravib31 <rkb0808@gmail.com>
2024-08-25 16:16:15 +0530

    rps complete

--- jsProjectsWithConcept/Booleans/booleans.js
+++ jsProjectsWithConcept/Booleans/booleans.js
@@ -1,2 +1,50 @@
-let compare = 3>5;
-console.log(compare);
+// let compare = 3>5;
+// console.log(compare);
+
+
+// Falsy Values - false, 0, "", null, undefined, NaN
+
+if(false){
+    console.log("I am truthy value");
+
+}
+else{
+    console.log("I am falsy value");
+}
+
+if(0){
+    console.log("I am truthy value");
+
+}
+else{
+    console.log("I am falsy value");
+}
+if(""){
+    console.log("I am truthy value");
+
+}
+else{
+    console.log("I am falsy value");
+}
+if(null){
+    console.log("I am truthy value");
+
+}
+else{
+    console.log("I am falsy value");
+}
+if(undefined){
+    console.log("I am truthy value");
+
+}
+else{
+    console.log("I am falsy value");
+}
+if(NaN){
+    // 'text'/5 -- gives us NaN 
+    console.log("I am truthy value");
+
+}
+else{
+    console.log("I am falsy value");
+}
---
+++ jsProjectsWithConcept/Rock-paper-scissors/rps.js
@@ -0,0 +1,73 @@
+const handleRock = () => {
+    let computerMove = '';
+    let result = "";
+    const randomNumber = Math.random();
+    if (randomNumber >= 0 && randomNumber < 1 / 3) {
+      computerMove = 'Rock';
+    } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
+      computerMove = 'Paper';
+    } else if (randomNumber >= 2 / 3 && randomNumber < 1) {
+      computerMove = 'Scissors';
+    }
+    // console.log(computerMove);
+    if(computerMove==="Rock"){
+      result = "It's a tie";
+    }
+    else if(computerMove==="Paper"){
+      result = "You lose";
+    }
+    else if(computerMove==="Scissors"){
+      result = "You win";
+    }
+    alert(`You picked Rock, computer picked ${computerMove}. ${result}!`)
+  };
+
+
+  const handlePaper = () => {
+    let computerMove = '';
+    let result = "";
+    const randomNumber = Math.random();
+    if (randomNumber >= 0 && randomNumber < 1 / 3) {
+      computerMove = 'Rock';
+    } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
+      computerMove = 'Paper';
+    } else if (randomNumber >= 2 / 3 && randomNumber < 1) {
+      computerMove = 'Scissors';
+    }
+    // console.log(computerMove);
+    if(computerMove==="Rock"){
+      result = "You win";
+    }
+    else if(computerMove==="Paper"){
+      result = " It's a tie";
+    }
+    else if(computerMove==="Scissors"){
+      result = "You lose";
+    }
+    alert(`You picked Paper, computer picked ${computerMove}. ${result}!`)
+  };
+
+
+  const handleScissors = () => {
+    let computerMove = '';
+    let result = "";
+    const randomNumber = Math.random();
+    if (randomNumber >= 0 && randomNumber < 1 / 3) {
+      computerMove = 'Rock';
+    } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
+      computerMove = 'Paper';
+    } else if (randomNumber >= 2 / 3 && randomNumber < 1) {
+      computerMove = 'Scissors';
+    }
+    // console.log(computerMove);
+    if(computerMove==="Rock"){
+      result = "You Loose";
+    }
+    else if(computerMove==="Paper"){
+      result = "You Win";
+    }
+    else if(computerMove==="Scissors"){
+      result = " It's a tie";
+    }
+    alert(`You picked Scissors, computer picked ${computerMove}. ${result}!`)
+  };

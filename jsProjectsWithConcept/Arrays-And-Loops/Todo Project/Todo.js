2be559e42a29ef1da1297af39292d5c50f20ea8b
ravib31 <rkb0808@gmail.com>
2024-08-29 00:34:11 +0530

    todo with date

--- jsProjectsWithConcept/Arrays-And-Loops/Todo Project/Todo.js
+++ jsProjectsWithConcept/Arrays-And-Loops/Todo Project/Todo.js
@@ -1,17 +1,41 @@
 const todoList = [];
-function addTodo(){
-    const inputElement = document.querySelector('.js-input');
-    const name = inputElement.value;
-    // console.log(name);
+function addTodo() {
+  const inputElement = document.querySelector(".js-input");
+  const inputDate = document.querySelector(".js-date");
+  const name = inputElement.value;
+  const date = inputDate.value;
+  console.log(name,date);
 
-    todoList.push(name);
-    console.log(todoList);
-    inputElement.value = '';
-    let finalList = [];
-    for(let i=0; i<todoList.length; i++){
-        console.log(todoList[i]);
-        finalList.push(`<li>${todoList[i]}</li>`);
-    }
-    console.log(finalList);
-    document.querySelector('.js-todoList').innerHTML = finalList;
+//   todoList.push(name + " " + date);
+  todoList.push({
+    name:name,
+    date:date
+});
+  console.log(todoList);
+  inputElement.value = "";
+  inputDate.value = "";
+  renderList();
+}
+// function clickHandle({i}) {
+//   console.log("clicked");
+//   todoList.splice(`${i}`,1)
+//   renderList();
+// }
+function renderList() {
+  let finalList = "";
+  for (let i = 0; i < todoList.length; i++) {
+    const todoName = todoList[i].name;
+    const todoDate = todoList[i].date;
+    console.log(todoName);
+    const html = `
+    <p>
+    ${todoName}  ${todoDate}
+    <button onClick='todoList.splice(${i},1);
+    renderList();
+    '>Delete</button></p>
+    `;
+    finalList = finalList + html;
+  }
+//   console.log(finalList);
+  document.querySelector(".js-todoList").innerHTML = finalList;
 }

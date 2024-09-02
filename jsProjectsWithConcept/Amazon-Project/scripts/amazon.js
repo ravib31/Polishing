ff1d243564c64d059b63e78784f902cdb8574e2f
ravib31 <rkb0808@gmail.com>
2024-09-03 00:33:35 +0530

    length updated in cart

--- jsProjectsWithConcept/Amazon-Project/scripts/amazon.js
+++ jsProjectsWithConcept/Amazon-Project/scripts/amazon.js
@@ -78,6 +78,12 @@ if(matchingItem){
     quantity: 1
   });
 }
-console.log(cart);
+let cartQuantity = 0;
+cart.forEach((item)=>{
+   cartQuantity += item.quantity;
+})
+document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+// console.log(cartQuantity);
+// console.log(cart);
   });
 });

a83b64962f6e7b3cba2c500129f301c2af83e238
ravib31 <rkb0808@gmail.com>
2024-09-03 22:19:00 +0530

    remove from cart

--- jsProjectsWithConcept/Amazon-Project/data/cart.js
+++ jsProjectsWithConcept/Amazon-Project/data/cart.js
@@ -1,15 +1,11 @@
-export const cart =[
-    {
-    productId:"e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity:2,
-    },
+export let cart =[
     {
         productId:"15b6fc6f-327a-4ec4-896f-486349e85a3d",
         quantity:1,
     },
     {
         productId:"83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
-        quantity:1,
+        quantity:2,
     },
    
 ];
@@ -34,3 +30,14 @@ export function addToCart(productId) {
     }
   }
   
+
+  export function removeFromCart(productId){
+    const newCart = [];
+    cart.forEach((cartItem) => {
+      if(cartItem.productId !== productId){
+        newCart.push(cartItem);
+      }
+    });
+    // console.log(newCart);
+    cart = newCart;
+  }

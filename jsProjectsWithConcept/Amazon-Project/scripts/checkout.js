1108648c671b285b56b557d1506ca8dab69108ca
ravib31 <rkb0808@gmail.com>
2024-09-04 17:03:18 +0530

    interactive delete from HTML when click on delete

--- jsProjectsWithConcept/Amazon-Project/scripts/checkout.js
+++ jsProjectsWithConcept/Amazon-Project/scripts/checkout.js
@@ -14,7 +14,7 @@ cart.forEach((cartItem) => {
   });
 
   cartSummaryHTML += `
-  <div class="cart-item-container">
+  <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
               Delivery date: Tuesday, June 21
             </div>
@@ -103,6 +103,9 @@ document.querySelectorAll(".js-delete-link").forEach((link) => {
     const productId = link.dataset.productId;
     // console.log(productId);
     removeFromCart(productId);
-    console.log(cart);
+    // console.log(cart);
+    const container = document.querySelector(`.js-cart-item-container-${productId}`);
+    console.log(container);
+    container.remove();
   });
 });

daf4b5593a223cda21af2bc81af2752e5470fb6a
ravib31 <rkb0808@gmail.com>
2024-09-09 22:19:46 +0530

    test 2

--- jsProjectsWithConcept/Amazon-Project/tests/moneyTest.js
+++ jsProjectsWithConcept/Amazon-Project/tests/moneyTest.js
@@ -1,7 +1,16 @@
-import { formatCurrency } from "../scripts/utils/money";
+import { formatCurrency } from "../scripts/utils/money.js";
 
 
+
+console.log("convert cents into dollars")
 if(formatCurrency(2095)==='20.95'){
+    console.log('Passed');
+}else{
+    console.log('Failed');
+}
+
+console.log("works with 0");
+if(formatCurrency(0)==='0.00'){
     console.log('Passed');
 }else{
     console.log('Failed');

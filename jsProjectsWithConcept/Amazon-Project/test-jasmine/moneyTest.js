30a3eb119e850df2486a24e75c9c1114d0651253
ravib31 <rkb0808@gmail.com>
2024-09-10 15:17:06 +0530

    remove default test and write new test

---
+++ jsProjectsWithConcept/Amazon-Project/test-jasmine/moneyTest.js
@@ -0,0 +1,13 @@
+import { formatCurrency } from "../scripts/utils/money.js";
+
+describe("test suite:formatCurrency",()=>{
+    it("converts cents into dollars",()=>{
+        expect(formatCurrency(2095)).toBe("20.95")
+    })
+    it("works with 0",()=>{
+        expect(formatCurrency(0)).toBe("0.00")
+    })
+    it("rounds up to the nearest cent",()=>{
+        expect(formatCurrency(2000.5)).toBe("20.01")
+    })
+})

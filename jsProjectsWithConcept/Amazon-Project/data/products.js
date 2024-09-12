00bb975ce92a5c1791a7854c9cdaee43b0504d39
ravib31 <rkb0808@gmail.com>
2024-09-12 18:11:33 +0530

    convert products in to class

--- jsProjectsWithConcept/Amazon-Project/data/products.js
+++ jsProjectsWithConcept/Amazon-Project/data/products.js
@@ -1,3 +1,37 @@
+import { formatCurrency } from "../scripts/utils/money.js";
+
+export function getProduct(productId){
+  let matchingProduct;
+  products.forEach((product) => {
+    if (product.id === productId) {
+      matchingProduct = product;
+    }
+  });
+  return matchingProduct;
+}
+
+class Product{
+  id;
+  image;
+  name;
+  rating;
+  priceCents;
+  constructor(productDetails){
+    this.id = productDetails.id;
+    this.image = productDetails.image;
+    this.name = productDetails.name;
+    this.rating = productDetails.rating;
+    this.priceCents = productDetails.priceCents;
+  }
+
+  getStarsUrl(){
+    return `images/ratings/rating-${this.rating.stars * 10}.png`;
+  }
+  getPrice(){
+    return `$${formatCurrency(this.priceCents)}`;
+  }
+}
+
 
 
 export const products = [
@@ -659,15 +693,13 @@ export const products = [
       "mens"
     ]
   }
-];
+].map((productDetails)=>{
+      return new Product(productDetails);
+});
+
+console.log(products)
 
 
-export function getProduct(productId){
-  let matchingProduct;
-  products.forEach((product) => {
-    if (product.id === productId) {
-      matchingProduct = product;
-    }
-  });
-  return matchingProduct;
-}
+
+
+

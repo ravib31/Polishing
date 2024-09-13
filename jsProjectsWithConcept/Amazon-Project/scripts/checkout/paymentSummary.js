d4e42745579368b52f63c41b946ad0d00a1ac499
ravib31 <rkb0808@gmail.com>
2024-09-13 18:04:31 +0530

    create clothings class

--- jsProjectsWithConcept/Amazon-Project/scripts/checkout/paymentSummary.js
+++ jsProjectsWithConcept/Amazon-Project/scripts/checkout/paymentSummary.js
@@ -9,13 +9,13 @@ let productPriceCents = 0;
 let shippingPriceCents = 0;
   cart.forEach((cartItem) => {
     const product = getProduct(cartItem.productId);
-    console.log(product);
+    // console.log(product);
     productPriceCents+=product.priceCents*cartItem.quantity;
     const deliveryOption = getDeliveryOption(cartItem.deliveryOptionId)
     shippingPriceCents+=deliveryOption.price;
   });
-  console.log(productPriceCents);
-  console.log(shippingPriceCents);
+  // console.log(productPriceCents);
+  // console.log(shippingPriceCents);
   const totalBeforeTaxCents = productPriceCents+shippingPriceCents;
   const taxCents = (totalBeforeTaxCents*0.1);
   const totalCents = totalBeforeTaxCents+taxCents;

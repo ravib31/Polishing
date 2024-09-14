c4985af25b2bff5a51d9bc27a111c04a3bc91479
ravib31 <rkb0808@gmail.com>
2024-09-15 01:16:07 +0530

    fetch products

--- jsProjectsWithConcept/Amazon-Project/data/products.js
+++ jsProjectsWithConcept/Amazon-Project/data/products.js
@@ -77,6 +77,29 @@ obj3.method();
 
 
 export let products = [];
+export function loadProductsFetch(){
+  const promise = fetch("https://supersimplebackend.dev/products")
+  .then((response)=>{
+    return response.json()
+   })
+ .then((data)=>{
+    // console.log(data);
+    products = data.map((productDetails)=>{
+      if(productDetails.type === "clothing"){
+        return new Clothing(productDetails);
+      }
+          return new Product(productDetails);
+    });
+    console.log("Loading products");
+  })
+  return promise;
+}
+
+// loadProductsFetch().then(()=>{
+//   console.log(products);
+// });
+
+
 export function loadProducts(fun){
   const xhr = new XMLHttpRequest();
   xhr.addEventListener("load",()=>{
--- jsProjectsWithConcept/Amazon-Project/scripts/checkout.js
+++ jsProjectsWithConcept/Amazon-Project/scripts/checkout.js
@@ -1,15 +1,11 @@
 import { loadCart } from "../data/cart.js";
-import { loadProducts } from "../data/products.js";
+import { loadProducts, loadProductsFetch } from "../data/products.js";
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 // import '../data/cart-class.js'
 
 Promise.all([
-    new Promise((resolve)=>{
-        loadProducts(()=>{
-            resolve('value1');
-        });
-    }),
+    loadProductsFetch(),
     new Promise((resolve)=>{
         loadCart(()=>{
             resolve();
